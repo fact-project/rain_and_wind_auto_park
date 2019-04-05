84a34c8265b81410d568ac97d8b09731b2c69243
Dominik Neise <neised@phys.ethz.ch>
2019-04-05 15:25:24 +0200

    write the main function for this program and write some comments for functions which we might need

--- rain_and_wind_auto_park/script.py
+++ rain_and_wind_auto_park/script.py
@@ -10,7 +10,95 @@ from docopt import docopt
 
 
 def main(**kwargs):
-    print('doing nothing yet, but my arguments are:', kwargs)
+
+    we_should_park = False
+    we_are_parking = False
+
+    while True:
+        rain_update = fetch_rain_sensor_update()
+        is_rainy = make_rain_decision(rain_update)
+
+        wind_update = fetch_wind_sensor_update()
+        is_stormy = make_storm_decision(wind_update)
+
+        we_should_park = is_rainy or is_stormy
+
+        if we_should_park and not we_are_parking:
+            enter_suspend_task_into_schedule()
+            we_are_parking = True
+        elif we_should_park and we_are_parking:
+            # nothing to be done
+            pass
+        elif not we_should_park and we_are_parking:
+            enter_resume_task_into_schedule()
+            we_are_parking = False
+        elif not we_should_park and not we_are_parking:
+            # nothing to be done
+            pass
+        else:
+            # there are only 4 combinations so this else should never happen
+            raise Exception('we should never reach this line!')
+
+
+def fetch_rain_sensor_update():
+    '''fetch new rain sensor data from tonights AUX file.
+    if there is no update, i.e. if there is no new line yet in the AUX file
+    return None
+
+    if there is a new line, return a row, from the astropy.Table object
+    '''
+    return None
+
+
+def make_rain_decision(rain_update):
+    '''make a decision if based on this update we assume it is rainy right now
+    or not.
+
+    Returns True if we assume it is rainy and False otherwise
+    '''
+    return False
+
+
+def fetch_wind_sensor_update():
+    '''fetch new wind sensor data from tonights AUX file.
+    if there is no update, i.e. if there is no new line yet in the AUX file
+    return None
+
+    if there is a new line, return a row, from the astropy.Table object
+    '''
+    return None
+
+
+def make_storm_decision(wind_update):
+    '''make a decision if based on this update we assume it is stormy right now
+    or not.
+
+    Returns True if we assume it is stormy and False otherwise
+    '''
+    return False
+
+
+def enter_suspend_task_into_schedule():
+    '''make an entry in the scheduling DB, which is represented on this website:
+    https://www.fact-project.org/schedule/
+
+    In particular, make a "SUSPEND" entry for "right now"
+    and in addition make a "RESUME" entry
+    after the most recent "SHUTDOWN" entry.
+    '''
+    pass
+
+
+def enter_resume_task_into_schedule():
+    '''make an entry in the scheduling DB, which is represented on this website:
+    https://www.fact-project.org/schedule/
+
+    In particular, make a "RESUME" entry for "right now"
+    and in addtion remove the "RESUME" task
+    after the most recent "SHUTDOWN" entry,
+    if it exists. It shoud exist. If it does not exist, print a warning.
+    '''
+    pass
 
 
 def entry():
