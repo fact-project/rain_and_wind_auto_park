1ca7500c7d84f0b909869ff52daaf13348b61f9a
Dominik Neise <neised@phys.ethz.ch>
2019-04-15 17:34:38 +0200

    make rain_collect_data.py --> more general using a new <name> parameter

--- collect_data.py
+++ collect_data.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python
 '''
-Program to collect all RAIN fits files and bundle them up in an h5 file.
-<out_file_name>    for example "foo.h5"
+Program to collect all <name> fits files and bundle them up in an h5 file.
+<out_file_name>    for example "rain.h5"
+<name>             static part of file name, e.g. RAIN
 <path_to_aux_folder>   the path where your fact aux file tree starts
 
+Example:
+    python collect_data.py ../aux RAIN_SENSOR_DATA rain.h5
+    python collect_data.py ../aux MAGIC_WEATHER_DATA wind.h5
+
 Usage:
-  rain_collect_data.py <path_to_aux_folder> <out_file_name>
+  collect_data.py <path_to_aux_folder> <name> <out_file_name>
 
 Options:
   -h --help     Show this screen.
   --version     Show version.
 '''
-import sys
 import glob
 from astropy.table import Table
 import pandas as pd
 from tqdm import tqdm
 from docopt import docopt
 
-def main(path_to_aux_folder, out_file_name):
+
+def main(path_to_aux_folder, name, out_file_name):
     '''collect all RAIN fits files from `path_to_aux_folder`
     and bundle them into an HDF5 file in `out_file_name`
     '''
 
     try:
-        glob_expression = path_to_aux_folder + "/*/*/*/*RAIN*"
+        glob_expression = path_to_aux_folder + "/*/*/*/*" + name + "*"
 
         frame = []
         for path in tqdm(sorted(glob.glob(glob_expression))):
@@ -42,9 +47,10 @@ def main(path_to_aux_folder, out_file_name):
 
 if __name__ == "__main__":
 
-    arguments = docopt(__doc__, version='rain_collect_data 0.1')
+    arguments = docopt(__doc__, version='collect_data 0.1')
 
     main(
         path_to_aux_folder=arguments['<path_to_aux_folder>'],
+        name=arguments['<name>'],
         out_file_name=arguments['<out_file_name>'],
     )
