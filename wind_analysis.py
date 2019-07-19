078a18df5d7598d31383587a43539f7093f6bc63
Dominik Neise <neised@phys.ethz.ch>
2019-07-19 11:27:47 +0200

    add centered parameter

--- wind_analysis.py
+++ wind_analysis.py
@@ -32,32 +32,12 @@ def load_wind_data(path):
 def wind_plots(data, outfile_name):
 
     figure, (ax1, ax2) = plt.subplots(
-        nrows=2,
-        sharex=True,
-        figsize=(30, 15),
-        gridspec_kw={"height_ratios": [3, 2]}
+        nrows=2, sharex=True, figsize=(30, 15), gridspec_kw={"height_ratios": [3, 2]}
     )
-    ax1.plot(
-        data.v_max,
-        ".",
-        label="Wind Gust",
-        alpha=0.5
-    )
-    ax1.plot(
-        data["quantile"],
-        ".:",
-        label="Rolling 95% Quantile"
-    )
-    ax1.plot(
-        data.quantile2,
-        ".:",
-        label="Rolling 90% Quantile"
-    )
-    ax1.plot(
-        data.quantile3,
-        ".:",
-        label="Rolling 70% Quantile, 30 min ",
-    )
+    ax1.plot(data.v_max, ".", label="Wind Gust", alpha=0.5)
+    ax1.plot(data.quantile1, ".:", label="Rolling 95% Quantile")
+    ax1.plot(data.quantile2, ".:", label="Rolling 90% Quantile")
+    ax1.plot(data.quantile3, ".:", label="Rolling 70% Quantile, 30 min ")
     ax1.axhspan(40, 50, facecolor="C1", alpha=0.2)
     ax1.set_ylabel("Wind Speed (km/h)", fontsize=18)
     ax1.legend(fontsize=18, loc="upper right")
@@ -68,74 +48,75 @@ def wind_plots(data, outfile_name):
         # the boolean here needs to be inverted!
         (1 - data.actual_observation) + 0,
         ".:",
-        label="Actual Schedule"
+        label="Actual Schedule",
     )
-    ax2.plot(
-        data.quantile_park + 2,
-        ".:",
-        label="95% Quantile Decision",
-    )
-    ax2.plot(
-        data.quantile_park2 + 4,
-        ".:",
-        label="90% Quantile Decision",
-    )
-    ax2.plot(
-        data.quantile_park3 + 6,
-        ".:",
-        label="70% Quantile Decision",
-    )
+    ax2.plot(data.quantile1_park + 2, ".:", label="95% Quantile Decision")
+    ax2.plot(data.quantile2_park + 4, ".:", label="90% Quantile Decision")
+    ax2.plot(data.quantile3_park + 6, ".:", label="70% Quantile Decision")
 
     ax2.legend(fontsize=18, loc="upper right")
     ax2.tick_params(axis="both", which="major", labelsize=18)
     ax2.set_yticks(list(range(8)))
-    ax2.set_yticklabels(['park', 'observe', 'park', 'observe', 'park', 'observe', 'park', 'observe'])
+    ax2.set_yticklabels(
+        ["park", "observe", "park", "observe", "park", "observe", "park", "observe"]
+    )
     ax2.grid()
 
     figure.savefig(outfile_name, dpi=300, figsize=(120, 120))
 
 
-
 def wind_main(
     input_data,
-    outfile_name,
+    outfile_name=None,
     start_time=None,
     end_time=None,
+    centered=False,
 ):
     data = load_wind_data(path=input_data)
-    data = data[start_time: end_time]
+    data = data[start_time:end_time]
 
-    data["quantile"] = data.v_max.rolling("1h").quantile(0.95)
-    data["quantile_park"] = calculate_hyst(data["quantile"], 40, 10)
+    tasks = [
+        ("quantile1", 60, 0.95, 40, 10),
+        ("quantile2", 60, 0.90, 40, 10),
+        ("quantile3", 30, 0.70, 40, 10),
+    ]
 
-    data["quantile2"] = data.v_max.rolling("1h").quantile(0.90)
-    data["quantile_park2"] = calculate_hyst(data["quantile2"], 40, 10)
+    if not centered:
+        for name, window, percent, hyst_min, hyst_width in tasks:
+            data[name] = data.v_max.rolling(window).quantile(percent)
+            data[name+"_park"] = calculate_hyst(data[name], hyst_min, hyst_width)
 
-    data["quantile3"] = data.v_max.rolling("30min").quantile(0.70)
-    data["quantile_park3"] = calculate_hyst(data["quantile3"], 40, 10)
+    else:
+        for name, window, percent, hyst_min, hyst_width in tasks:
+            data[name] = data.v_max.rolling(min_periods=1, center=True, window=window).quantile(percent)
+            data[name+"_park"] = calculate_hyst(data[name], hyst_min, hyst_width)
 
-    total_hours = len(data.planned_observation) / 60
 
-    print("total_hours:", total_hours)
+    hours_of_planned_observation = data.planned_observation.sum() / 60
+
     data = data[data.planned_observation]
 
     if outfile_name:
         wind_plots(data, outfile_name)
 
-    data['park'] = data.quantile_park3
+    data["park"] = data.quantile_park3
     interval_lengths = intervals(data.park)
     result = get_no_small_intervals(interval_lengths)
-    print('intervals result:', result)
-    return result
+
+    return result, hours_of_planned_observation
 
 
 if __name__ == "__main__":
 
     arguments = docopt(__doc__, version="wind_analysis")
 
-    wind_main(
+
+    result, hours_of_planned_observation = wind_main(
         input_data=arguments["<input_data>"],
         outfile_name=arguments["-o"],
         start_time=arguments["--start"],
         end_time=arguments["--end"],
+        centered=arguments["--centered"]
     )
+    print("intervals result:", result)
+    print("hours of planned observation:", hours_of_planned_observation)
