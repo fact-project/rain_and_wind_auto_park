3f3413b4ed26e0123e83f9f869f369a074c47c37
Dominik Neise <neised@phys.ethz.ch>
2019-07-23 12:06:20 +0200

    use AuxService to read the fits files

--- fits_to_h5.py
+++ fits_to_h5.py
@@ -17,6 +17,7 @@ from astropy.table import Table
 import pandas as pd
 from tqdm import tqdm
 from docopt import docopt
+from fact.auxservices import AuxService
 
 def main(path_to_aux_folder, out_file_name, type_name):
     '''collect all RAIN fits files from `path_to_aux_folder`
@@ -24,32 +25,26 @@ def main(path_to_aux_folder, out_file_name, type_name):
 
     type_name should be either "RAIN" or "WEATHER"
     '''
+    glob_expression = path_to_aux_folder + f"/*/*/*/*{type_name}*"
 
-    try:
-        glob_expression = path_to_aux_folder + f"/*/*/*/*{type_name}*"
+    frames = []
+    for path in tqdm(sorted(glob.glob(glob_expression))):
+        try:
+            dataframe = AuxService.read_file(path)
+        except Exception as e:
+            print(f'error in {path}')
+        frames.append(dataframe)
 
-        frame = []
-        for path in tqdm(sorted(glob.glob(glob_expression))):
-            table = Table.read(path)
-            dataframe = table.to_pandas()
-            frame.append(dataframe)
+    result = pd.concat(frames, sort=False)
+    result.sort_values('Time', inplace=True)
+    result.set_index(
+        pd.to_datetime(result.Time, unit="D"),
+        inplace=True
+    )
+    result.sort_index(inplace=True)  # this should not be necessary
+    del result['Time']
 
-        result = pd.concat(frame)
-
-        result.sort_values('Time', inplace=True)
-
-        result.set_index(
-            pd.to_datetime(result.Time, unit="D"),
-            inplace=True
-        )
-        result.sort_index(inplace=True)
-        del result['Time']
-
-        result.to_hdf(out_file_name, key='data')
-
-
-    except ValueError:
-        print('no fits files found, maybe wrong path?')
+    result.to_hdf(out_file_name, key='data')
 
 
 if __name__ == "__main__":
